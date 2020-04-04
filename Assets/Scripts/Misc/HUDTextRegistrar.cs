4a3ab4384b7a9453535770ab3348c191302417ed
Aendur <Aendur@users.noreply.github.com>
2020-04-04 15:59:51 -0300

    UI Draft 2

---
+++ Assets/Scripts/Misc/HUDTextRegistrar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDTextRegistrar : MonoBehaviour
+{
+    [SerializeField]
+    private HP playerHP;
+    [SerializeField]
+    private Energy playerEnergy;
+
+    private UnityEngine.UI.Text text;
+
+    public enum ValueField { undefined, HP, Energy };
+    public ValueField valueField = ValueField.undefined;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<UnityEngine.UI.Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(valueField)
+        {
+            case ValueField.HP:
+                text.text = ((int) playerHP.hp).ToString();
+                break;
+            case ValueField.Energy:
+                text.text = ((int) playerEnergy.energy).ToString();
+                break;
+            default:
+                text.text = "???";
+                break;
+        }
+        
+    }
+}
