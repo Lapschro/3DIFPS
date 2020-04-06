a3a5ba86ee3139b8369c2f3735add03f4bde8d09
Aendur <Aendur@users.noreply.github.com>
2020-04-05 23:29:49 -0300

    started working on waiting room

---
+++ Assets/Scripts/Misc/Countdown.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Countdown : MonoBehaviour
+{
+    public UnityEngine.UI.Text countdownText;
+    public float countdownTime;
+
+    float startTime;
+    float remainingTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.realtimeSinceStartup;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        remainingTime = Mathf.Clamp(countdownTime - (Time.realtimeSinceStartup - startTime), 0.0f, countdownTime);
+        countdownText.text = ((int) remainingTime).ToString("D2");
+
+        if (remainingTime <= 0.0f)
+        {
+            // Let the revels begin
+        }
+    }
+}
