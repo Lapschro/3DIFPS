af693fa7768f5c3e8196a51eb707e05125dd8c0b
Aendur <Aendur@users.noreply.github.com>
2020-04-06 12:14:57 -0300

    Credits

---
+++ Assets/Scripts/Misc/HUDCredits.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+
+public class HUDCredits : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject credUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            Confirm();
+        }
+    }
+
+    public void Confirm()
+    {
+        credUI.SetActive(false);
+    }
+    private void OnEnable()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    private void OnDisable()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
